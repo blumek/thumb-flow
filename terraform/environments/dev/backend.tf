9b7c92cf480244e7d3569e0861f3ff60764068c1
Szymon Blumczynski <szymon.blumczynski@gmail.com>
2025-08-01 23:09:08 +0200

    Change terraform backend

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "thumbflow-terraform-state"
+    bucket = "infra-thumbflow-terraform-state"
     key    = "environments/dev/terraform.tfstate"
     region = "us-east-1"
   }
